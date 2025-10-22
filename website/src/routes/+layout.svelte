f7b9b6b649d497ce80ba75f4c0f2388188479645
Exr0n <mail@exr0n.com>
2025-10-22 13:03:53 -0700

    title and favicon

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script>
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/wavetable_logo.png';
 	
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>The Wavetable Project</title>
 </svelte:head>
 
 {@render children?.()}
