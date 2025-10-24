8c9d3439ebb5a0a4decc6b473ce0eec8657d27b9
Exr0n <mail@exr0n.com>
2025-10-23 17:05:59 -0700

    initial mainpage content

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -6,6 +6,10 @@
     import Mainpage from '$lib/text/mainpage.svx';
 </script>
 
+<svelte:head>
+    <title>The Wavetable Project</title>
+</svelte:head>
+
 <div class="flex flex-col">
     <!-- hero -->
     <div class="md:h-[60vh] rounded-md relative w-full md:w-max mx-auto">
@@ -13,7 +17,7 @@
         <div class="absolute left-0 bottom-0 md:max-w-1/3 space-y-2 md:space-y-4 translate-y-8 md:translate-y-0">
             <h1 class="text-2xl xs:text-4xl sm:text-[2.5rem] md:text-5xl font-bold">The<br>Wavetable<br>Project</h1>
             <h2 class="text-xs md:text-xl">
-                <span class="inline-block">$120 ocean/atmosphere</span>
+                <span class="inline-block">Tabletop ocean/atmosphere</span>
                 <span class="inline-block">physics simulator</span>
             </h2>
         </div>
@@ -32,10 +36,9 @@
             <img src={learn_img} alt="build a wavetable" class="m-auto slate-y-[20%] scale-90 opacity-10 hover:opacity-30 transition-all relative" />
             <h3 class="absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] font-bold pointer-events-none">Learn&nbsp;⮕</h3>
         </a>
-        <!-- TODO: big nav buttons with pictures: exploded view for construction, photo of experiments/ppl for usage, and experients-->
     </div>
 
-    <div class="prose max-w-none mt-8">
+    <div class="prose max-w-none mt-8 mb-32">
         <Mainpage />
     </div>
 </div>
